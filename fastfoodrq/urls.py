1694037b4361981c21da1aac53a38e3fd75e1f84
Nicholas Kegler <nicholasks@gmail.com>
2018-10-10 20:25:59 -0300

    Added Django REST Framework

--- fastfoodrq/urls.py
+++ fastfoodrq/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ordering/', include('ordering.urls')),
+    path('ordering/', include('fastfoodrq.ordering.urls')),
+    path('api/', include('fastfoodrq.api.urls')),
 ]
