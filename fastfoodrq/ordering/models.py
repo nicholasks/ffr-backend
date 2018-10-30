4265aa78e93dd4cbcc822a0bbb6a10be247a70e4
Nicholas Kegler <nicholasks@gmail.com>
2018-10-30 19:05:22 -0300

    Basic product added

--- fastfoodrq/ordering/models.py
+++ fastfoodrq/ordering/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """
+    Describe and define a product, it can be a food or a drink.
+    """
+
+    name = models.CharField(
+        max_lenght=32,
+        unique=True,
+        null=False,
+    )
+    description = models.CharField(
+        max_lenght=128,
+        null=True,
+        blank=True,
+    )
