eca36145a8f8aa789336f94a81c823668262b55a
Nicholas Kegler <nicholasks@gmail.com>
2018-11-08 00:53:36 -0200

    Serving product's image on development.

--- fastfoodrq/urls.py
+++ fastfoodrq/urls.py
@@ -15,9 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ordering/', include('fastfoodrq.ordering.urls')),
     path('api/', include('fastfoodrq.api.urls')),
 ]
+
+
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    from django.conf.urls.static import static
+
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
