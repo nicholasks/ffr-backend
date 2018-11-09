0d9c71c3b1aec6e4edc3196c477cb63dcbfc6223
Nicholas Kegler <nicholasks@gmail.com>
2018-11-09 00:33:20 -0200

    Better formating for Serializers.

--- fastfoodrq/api/serializers.py
+++ fastfoodrq/api/serializers.py
@@ -10,72 +10,54 @@ from fastfoodrq.ordering.models import (
 )
 
 
-class ProductSerializer(serializers.HyperlinkedModelSerializer):
-    category = serializers.SlugRelatedField(
-        many=False,
-        read_only=True,
-        slug_field='name'
-    )
-    ingredients = serializers.SlugRelatedField(
-        many=True,
-        read_only=True,
-        slug_field='name'
-    )
-    tags = serializers.SlugRelatedField(
-        many=True,
-        read_only=True,
-        slug_field='name'
-    )
-
-    class Meta:
-        model = Product
-        fields = (
-            'name',
-            'description',
-            'category',
-            'ingredients',
-            'tags',
-            'price',
-            'price_promo',
-            'image',
-        )
-
-
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = Category
-        fields = (
-            'name',
-            'description',
-            'parent',
-        )
+        fields = ('__all__')
 
 
 class IngredientSerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = Ingredient
         fields = ('__all__')
 
 
 class TagSerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = Tag
-        fields = ('name', 'description')
+        fields = ('__all__')
+
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+
+    category = CategorySerializer(many=False, read_only=True)
+    ingredients = IngredientSerializer(many=True)
+    tags = TagSerializer(many=True)
+
+    class Meta:
+        model = Product
+        fields = ('__all__')
 
 
 class TabSerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = Tab
         fields = ('__all__')
 
 
 class OrderSerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = Order
         fields = ('__all__')
 
 
 class OrderItemSerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = OrderItem
         fields = ('__all__')
