d14189ee26f774d1c8aedf8d06f4b08791c84c8d
Nicholas Kegler <nicholasks@gmail.com>
2018-12-05 23:52:20 -0200

    Django tables and showing products for kitchen.

--- fastfoodrq/ordering/views.py
+++ fastfoodrq/ordering/views.py
@@ -1,5 +1,6 @@
 import django_tables2 as tables
 
+from django.db.models import Q
 from django.shortcuts import render
 from django_tables2 import RequestConfig
 from .models import Order, OrderItem
@@ -9,9 +10,19 @@ class OrderItemTable(tables.Table):
     class Meta:
         model = OrderItem
         template_name = 'django_tables2/bootstrap.html'
+        exclude = ('is_promo', 'total_price', 'unity_price',)
 
 
 def index(request):
-    table = OrderItemTable(OrderItem.objects.all())
-    RequestConfig(request).configure(table)
-    return render(request, 'simple_list.html', {'orders': table})
+    orders = Order.objects.all().filter(Q(status='pe') | Q(status='pr'))
+    tb_orderItem = OrderItemTable(OrderItem.objects.all())
+    tb_order = OrderTable(orders)
+
+    RequestConfig(request).configure(tb_orderItem)
+    return render(request, 'simple_list.html', {'order': tb_order, 'orderitem': tb_orderItem})
+
+
+class OrderTable(tables.Table):
+    class Meta:
+        model = Order
+        template_name = 'django_tables2/bootstrap.html'
